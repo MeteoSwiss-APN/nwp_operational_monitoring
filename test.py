a3dee2d801f336a6e94ad55035a38c7c28a37f56
cghielmini <chiara.ghielmini@meteoswiss.ch>
2026-07-15 13:28:38 +0200

    Refactor late task monitoring and symlink creation

--- test.py
+++ test.py
@@ -3,30 +3,94 @@ from ecflow import *
 
 home = os.path.join(os.getenv("HOME"), "course")
 
+scripts = os.path.join(home, "scripts")
+
+# Create symlinks for the generic notify script
+tasks_to_monitor = [
+    ("t1", "main"),
+    ("t2", "main"),
+    ("t3", "main"),
+    ("t4", "second"),
+]
+
+for task, _ in tasks_to_monitor:
+    target = os.path.join(scripts, "notify_late.ecf")
+    link = os.path.join(scripts, f"{task}_late_monitor.ecf")
+
+    if os.path.lexists(link):
+        os.remove(link)
+
+    os.symlink(target, link)
+
+
+
+def late_monitor(task, family):
+    return Task(
+        f"{task}_late_monitor",
+        Trigger(f"{task}<flag>late"),
+        Edit(
+            TASK_PATH=f"/test/{family}/{task}"
+        )
+    )
 
 defs = Defs(
 Suite("test",
 
     Family("main",
+        Task("main_start", Edit(SKIP_SPAN="1")),
 
         Task(
             "t1",
+            Trigger("main_start == complete"),
             Label("duration", ""),
-            Late(complete="00:01"),
-            Meter("progress", 0, 50)
+            Late(complete="+00:01")
 
         ),
 
         Task(
             "t2",
+            Trigger("t1 == complete"),
             Label("duration", ""),
-            Late(complete="00:01")
+            Late(complete="+00:01")
         ),
 
         Task(
-            "notify_late",
-            Trigger("t2<flag>late")
+            "t3",
+            Trigger("t2 == complete"),
+            Label("duration", ""),
+            Late(complete="+00:01")
         ),
+
+        Task(
+             "main_end",
+             Trigger("t3 == complete"),
+             Edit(SKIP_SPAN="1",FAMILY_PATH="/test/main")
+         ),
+        late_monitor("t1", "main"),
+        late_monitor("t2", "main"),
+        late_monitor("t3", "main"),
+
+
+    ),
+
+    Family("second",
+        Trigger("/test/main/main_end == complete"),
+        Task("second_start", Edit(SKIP_SPAN="1")),
+        
+        Task(
+            "t4",
+            Label("duration", ""),
+            Late(complete="+00:01")
+
+        ),
+        Task(
+            "second_end",
+            Trigger("t4 == complete"),
+            Edit(SKIP_SPAN="1", FAMILY_PATH="/test/second")
+        ),
+
+        late_monitor("t4", "second"),
+
 
     ),
 
